01bc239868f384e0179a8f4dfbfa488230770724
Craig Barnes <Cr@igBarn.es>
2019-10-20 17:01:48 +0100

    Print output from examples/longest-sequence.lua in sorted order

--- examples/longest-sequence.lua
+++ examples/longest-sequence.lua
@@ -1,6 +1,6 @@
 local terminfo = require "terminfo-parser"
 local terms = assert(terminfo.parse_file("terminfo.src"))
-local max_lengths = {}
+local lengths = {}
 
 local caps = {
     "op", "sgr0", "rmkx", "smkx", "rmcup", "smcup", "cnorm", "civis",
@@ -11,13 +11,17 @@ for i, entry in ipairs(terms) do
         local seq = entry[cap]
         if seq then
             local seq_len = #seq
-            if seq_len > (max_lengths[cap] or 0) then
-                max_lengths[cap] = seq_len
+            if seq_len > (lengths[cap] or 0) then
+                lengths[cap] = seq_len
             end
         end
     end
 end
 
-for k, v in pairs(max_lengths) do
-    print(k, v)
+table.sort(caps, function(a, b)
+    return lengths[a] > lengths[b]
+end)
+
+for i, str in ipairs(caps) do
+    io.write(("%4u  %s\n"):format(lengths[str], str))
 end
