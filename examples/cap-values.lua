3a7d99f92a38b1abd725ffd3672018e0f6c53e61
Craig Barnes <craigbarnes@protonmail.com>
2024-10-27 08:00:42 +0000

    Add `-v` (verbose) command-line option to `examples/cap-values.lua`

--- examples/cap-values.lua
+++ examples/cap-values.lua
@@ -3,16 +3,20 @@ local escape = assert(terminfo.escape)
 local write = assert(io.write)
 local terms = assert(terminfo.parse_file("terminfo.src"))
 local counts, index, n = {}, {}, 0
-local capname = arg[1]
+local verbose = (arg[1] == "-v")
+local capname = arg[verbose and 2 or 1]
 
 if not capname then
-    io.stderr:write("Usage: ", arg[0], " CAP-NAME\n")
+    io.stderr:write("Usage: ", arg[0], " [-v] CAP-NAME\n")
     os.exit(1)
 end
 
 for term, caps in terms:iter() do
     local val = caps[capname]
     if val then
+        if verbose then
+            write(("%5s   %s\n"):format(escape(val), term))
+        end
         local count = counts[val]
         if count then
             counts[val] = count + 1
@@ -32,6 +36,10 @@ end
 table.sort(index, function(a, b)
     return counts[a] > counts[b]
 end)
+
+if verbose then
+    write("\nTotals\n-----------\n")
+end
 
 for i = 1, n do
     local str = index[i]
