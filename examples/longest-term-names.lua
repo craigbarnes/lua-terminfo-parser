f472f237cdab01be71ae905e48aec2e81d0fa2c4
Craig Barnes <craigbarnes@protonmail.com>
2026-08-10 22:20:01 +0100

    Use stable sort in `examples/longest-term-names.lua`

--- examples/longest-term-names.lua
+++ examples/longest-term-names.lua
@@ -13,8 +13,10 @@ for _, caps in terms:iter() do
     end
 end
 
-table.sort(names, function(a, b)
-    return #a > #b
+table.sort(names, function(s1, s2)
+    -- Sort descending by length, then ascending by name (for stable ordering)
+    local len1, len2 = #s1, #s2
+    return len1 == len2 and s1 < s2 or len1 > len2
 end)
 
 local limit = math.min(tonumber(arg[1]) or 10, #names)
